9ec7772f190ce0725d0c3f7edd206669b0b5ac94
AndreX Network <121361241+ax-col@users.noreply.github.com>
2025-03-12 21:07:06 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = 'raspa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/favicon.ico',
+  '/android-icon-192x192.png',
+  '/apple-icon-180x180.png',
+  // Agrega aquí otros archivos que quieras cachear
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
