96988cb8db4fd142851119271da35fe645397611
AndreX Network <121361241+ax-col@users.noreply.github.com>
2025-03-12 19:45:26 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const cursor = document.querySelector('.cursor');
+
+// Posicionar el cursor en el centro al cargar la página
+window.addEventListener('load', () => {
+  cursor.style.left = '50%';
+  cursor.style.top = '50%';
+});
+
+// Mover el cursor con el ratón
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
+
+// Cambiar tamaño al hacer clic
+document.addEventListener('mousedown', () => {
+  cursor.style.width = '25px';
+  cursor.style.height = '25px';
+});
+
+document.addEventListener('mouseup', () => {
+  cursor.style.width = '30px';
+  cursor.style.height = '30px';
+});
